bbd0cbbaa454c4220fdcc484b387f9732a826c92
Oscar Ochoa <97921525+bustyAI@users.noreply.github.com>
2024-10-21 19:13:27 -0700

    creating Navbar component + animation

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../globals.css";
+
+export default function Navbar() {
+  return (
+    <div className=" flex justify-center items-center w-full h-24 bg-purple-400 animate-slideDown">
+      <h3 className=" m-0">Welcome</h3>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,21 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        slideDown: "slideDown 1.5s ease-in-out forwards",
+      },
+      keyframes: {
+        slideDown: {
+          from: {
+            transform: "translateY(-100%)",
+            opacity: 0,
+          },
+          to: {
+            transform: "translateY(0)",
+            opacity: 100,
+          },
+        },
+      },
     },
   },
   plugins: [],
